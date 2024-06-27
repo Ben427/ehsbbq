98e7b8e24af7023a48259a2325128d95e9db9661
Ben <b.bordofsky@gmail.com>
2024-06-26 21:58:39 -0700

    Fixed link opening in new tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,7 +71,7 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-container">
               <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/img/edmondsbbq_menu.pdf" target="_blank">
+              <Link className="navbar-item" to="https://edmondsbbq.com/img/edmondsbbq_menu.pdf" target="_blank" rel="noopener noreferrer">
                 Menu
               </Link>
               <Link className="navbar-item" to="#location">
